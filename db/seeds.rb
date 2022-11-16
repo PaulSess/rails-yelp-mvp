315cec1994185adbfab7f6c2adb3086317dd52ed
Paul Sessbruegger <paul.at.home@gmx.de>
2022-11-16 16:55:49 +0100

    livecode break

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,15 @@
-
-
-
+require 'faker'
+puts "first all old seeds get deleted.."
+Restaurant.destroy_all
 
 5.times do
-  new_restaurant = Restaurant.new(name:
-                                  address:
-                                  phone_number:
+  new_restaurant = Restaurant.new(name: Faker::Restaurant.name,
+                                  address: Faker::Address.city,
+                                  phone_number: Faker::PhoneNumber.phone_number,
                                   category: ["chinese", "italian", "japanese", "french", "belgian"].sample, )
   new_restaurant.save
 end
+puts "new seeds got generated!"
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
